831aa845ecb99643ec645a08d59a98cfa0a83471
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2023-01-23 09:43:56 -0600

    algoritmo BFS cambiado para referencia

--- BFS.py
+++ BFS.py
@@ -1,31 +1,136 @@
 """
-Referencia: https://www.educative.io/answers/how-to-implement-a-breadth-first-search-in-python
+Referencia: https://www.youtube.com/watch?v=hettiSrJjM4&ab_channel=TechWithTim
 """
 
-graph = {
-  'A' : ['B','C'],
-  'B' : ['D', 'E'],
-  'C' : ['F'],
-  'D' : [],
-  'E' : ['F'],
-  'F' : []
-}
+import queue
 
-visited = [] # List to keep track of visited nodes.
-queue = []     #Initialize a queue
 
-def bfs(visited, graph, node):
-  visited.append(node)
-  queue.append(node)
+def createMaze():
+    maze = []
+    maze.append(["#","#", "#", "#", "#", "O","#"])
+    maze.append(["#"," ", " ", " ", "#", " ","#"])
+    maze.append(["#"," ", "#", " ", "#", " ","#"])
+    maze.append(["#"," ", "#", " ", " ", " ","#"])
+    maze.append(["#"," ", "#", "#", "#", " ","#"])
+    maze.append(["#"," ", " ", " ", "#", " ","#"])
+    maze.append(["#","#", "#", "#", "#", "X","#"])
 
-  while queue:
-    s = queue.pop(0) 
-    print (s, end = " ") 
+    return maze
 
-    for neighbour in graph[s]:
-      if neighbour not in visited:
-        visited.append(neighbour)
-        queue.append(neighbour)
+def createMaze2():
+    maze = []
+    maze.append(["#","#", "#", "#", "#", "O", "#", "#", "#"])
+    maze.append(["#"," ", " ", " ", " ", " ", " ", " ", "#"])
+    maze.append(["#"," ", "#", "#", " ", "#", "#", " ", "#"])
+    maze.append(["#"," ", "#", " ", " ", " ", "#", " ", "#"])
+    maze.append(["#"," ", "#", " ", "#", " ", "#", " ", "#"])
+    maze.append(["#"," ", "#", " ", "#", " ", "#", " ", "#"])
+    maze.append(["#"," ", "#", " ", "#", " ", "#", "#", "#"])
+    maze.append(["#"," ", " ", " ", " ", " ", " ", " ", "#"])
+    maze.append(["#","#", "#", "#", "#", "#", "#", "X", "#"])
 
-# Driver Code
-bfs(visited, graph, 'F')
+    return maze
+
+
+def printMaze(maze, path=""):
+    for x, pos in enumerate(maze[0]):
+        if pos == "O":
+            start = x
+
+    i = start
+    j = 0
+    pos = set()
+    for move in path:
+        if move == "L":
+            i -= 1
+
+        elif move == "R":
+            i += 1
+
+        elif move == "U":
+            j -= 1
+
+        elif move == "D":
+            j += 1
+        pos.add((j, i))
+    
+    for j, row in enumerate(maze):
+        for i, col in enumerate(row):
+            if (j, i) in pos:
+                print("+ ", end="")
+            else:
+                print(col + " ", end="")
+        print()
+        
+
+
+def valid(maze, moves):
+    for x, pos in enumerate(maze[0]):
+        if pos == "O":
+            start = x
+
+    i = start
+    j = 0
+    for move in moves:
+        if move == "L":
+            i -= 1
+
+        elif move == "R":
+            i += 1
+
+        elif move == "U":
+            j -= 1
+
+        elif move == "D":
+            j += 1
+
+        if not(0 <= i < len(maze[0]) and 0 <= j < len(maze)):
+            return False
+        elif (maze[j][i] == "#"):
+            return False
+
+    return True
+
+
+def findEnd(maze, moves):
+    for x, pos in enumerate(maze[0]):
+        if pos == "O":
+            start = x
+
+    i = start
+    j = 0
+    for move in moves:
+        if move == "L":
+            i -= 1
+
+        elif move == "R":
+            i += 1
+
+        elif move == "U":
+            j -= 1
+
+        elif move == "D":
+            j += 1
+
+    if maze[j][i] == "X":
+        print("Found: " + moves)
+        printMaze(maze, moves)
+        return True
+
+    return False
+
+
+# MAIN ALGORITHM
+
+nums = queue.Queue()
+nums.put("")
+add = ""
+maze  = createMaze2()
+
+while not findEnd(maze, add): 
+    add = nums.get()
+    #print(add)
+    for j in ["L", "R", "U", "D"]:
+        put = add + j
+        if valid(maze, put):
+            nums.put(put)
